a704875a85de71a190d839aa0af1a38bf613b05f
Ermir Beqiraj <ermirbeqiraj@hotmail.com>
2020-01-18 10:45:28 +0100

    Add project files.

---
+++ JsonRpc.API/Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+﻿using EdjCase.JsonRpc.Router;
+
+namespace JsonRpc.API.Controllers
+{
+    public class ItemsController : RpcController
+    {
+        public Item Get()
+        {
+            var item = new Item(3, "Ermir");
+            return item;
+        }
+
+        public void Add(Item item)
+        {
+            //Adds item
+        }
+    }
+
+    public class Item {
+        public Item(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+    }
+}
---
+++ JsonRpcConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using EdjCase.JsonRpc.Client;
+using EdjCase.JsonRpc.Core;
+using System;
+
+namespace JsonRpcConsole
+{
+    public class Item
+    {
+        public Item(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+    }
+
+    class Program
+    {
+        //https://github.com/edjCase/JsonRpc
+
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:59385/Items/";
+            IRpcTransportClient transportClient = new HttpRpcTransportClient();
+            RpcClient client = new RpcClient(new Uri(url), transportClient: transportClient);
+            RpcRequest request = RpcRequest.WithParameterList("Get", null, "Id1");
+            RpcResponse<Item> response = client.SendRequestAsync<Item>(request).GetAwaiter().GetResult();
+
+            Console.WriteLine($"response.Result.Id: {response.Result.Id} | response.Result.Name: {response.Result.Name}");
+        }
+    }
+}
